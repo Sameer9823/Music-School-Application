c2c12e195bdaec641b0f9c7af742adfb036957d9
Sameer9823 <sameerselokar924@gmail.com>
2024-07-22 11:03:54 +0530

    Scroll-Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedCourses from "@/Components/FeaturedCourses";
 import HeroSection from "@/Components/HeroSection";
+import WhyChooseUs from "@/Components/WhyChooseUs";
 
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
       <FeaturedCourses/>
+      <WhyChooseUs/>
     </main>
   );
 }
